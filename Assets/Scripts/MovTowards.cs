090778e33b2a23f6a672e39b0ad007c39b25d4a1
JavierJasso23 <javierjasso321@gmail.com>
2021-11-07 19:09:41 -0600

    P2 Update_2

--- Assets/Scripts/MovTowards.cs
+++ Assets/Scripts/MovTowards.cs
@@ -6,71 +6,79 @@ public class MovTowards : MonoBehaviour
 {
     [SerializeField]
     float speed = 10;
+    [SerializeField]
+    GameObject ObjDestino;
+    [SerializeField]
+    GameObject ObjAMover;
 
-    int timer = 0;
     Vector3 origen;
     Vector3 destino;
+    int timer = 0;
+    bool activo;
+    float distancia = 0.0f; 
 
     // Start is called before the first frame update
     void Start()
     {
-        empezar();
-    }
-    void empezar()
-    {
-        StartCoroutine("CorrutinaBot");
+        activo = true;
     }
 
     // Update is called once per frame
-    /*void Update()
-    {        
-        Vector3 origen = transform.position;
-        Vector3 destino = ObjDestino.transform.position;
-        
-        transform.LookAt(destino);
+    void Update()
+    {
+        if (activo)
+        {
+            Vector3 origen = ObjAMover.transform.position;
+            Vector3 destino = ObjDestino.transform.position;
 
-        destino.x -= 10.0f;
-        //destino.z -= 10.0f;
-        
+            ObjAMover.transform.LookAt(destino);
 
-        transform.position =  Vector3.MoveTowards(origen, destino, speed * Time.deltaTime);
+            destino.x -= 5.0f;
+            //destino.z -= 10.0f;
+            destino.y = 0.1f;
+            origen.y = 0.1f;
 
-        //transform.position = Vector3.Lerp(origen, destino, speed * Time.deltaTime);
+            ObjAMover.transform.position = Vector3.MoveTowards(origen, destino, speed * Time.deltaTime);
 
-        //Vector3 currentVelocity = new Vector3(0f, 0f, 0f);
-        //transform.position = Vector3.SmoothDamp(origen, destino,ref currentVelocity, speed * Time.deltaTime);
+            //transform.position = Vector3.Lerp(origen, destino, speed * Time.deltaTime);
 
-        float distancia = Vector3.Distance(origen, destino);
-        Debug.Log(distancia);
+            //Vector3 currentVelocity = new Vector3(0f, 0f, 0f);
+            //transform.position = Vector3.SmoothDamp(origen, destino,ref currentVelocity, speed * Time.deltaTime);
 
-    }*/
+            distancia = Vector3.Distance(origen, destino);
+            Debug.Log(distancia);
+            if (distancia < 2.0f)
+            {
+                if (ObjAMover.transform.position.x < ObjDestino.transform.position.x - 5)
+                {
+                    activo = false;
+                    StartCoroutine("corrutinaMov");
+                }
+            }
+        }
+        if (ObjDestino.transform.position.z > 83)
+        {
+            activo = false;
+            StopCoroutine("corrutinaMov");
+        }
 
-    IEnumerator CorrutinaBot()
+    }
+
+    IEnumerator corrutinaMov()
     {
         WaitForSeconds Wait = new WaitForSeconds(1.0f);
+
         while (true)
         {
             timer++;
-
-            if (timer == 5)
+            if (timer == 7)
             {
-                transform.LookAt(destino);
-                destino.x -= 10.0f;
-                transform.position = Vector3.MoveTowards(origen, destino, speed * Time.deltaTime);
-            }
-
-            if (timer == 9)
-            {
+                activo = true;
                 timer = 0;
-            }
-
-            if (this.transform.position.z > 85.0f)
-            {
-                StopCoroutine("CorrutinaTimer");
+                StopCoroutine("corrutinaMov");
             }
 
             yield return Wait;
         }
     }
-
 }
